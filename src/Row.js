ceabd9712c59471c8c925c6d2bc32049f790092e
ggsuccess <ggsuccess@naver.com>
2020-07-18 16:20:00 +0900

    fetch API call test

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import './Row.css';
+
+const base_url = 'https://image.tmdb.org/t/p/original/';
 
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
@@ -9,13 +12,21 @@ function Row({ title, fetchUrl }) {
       setMovies(request.data.results);
       return request;
     };
-
     fetchData();
   }, [fetchUrl]);
+  console.table(movies);
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className=""></div>
+      <div className="row-posters">
+        {movies.map((movie) => (
+          <img
+            className="row-poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
     </div>
   );
 }
