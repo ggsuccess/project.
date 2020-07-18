2d33ee950b061e081e206c6f235fa9f662658bb0
ggsuccess <ggsuccess@naver.com>
2020-07-18 18:40:57 +0900

    movies genre

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ const App = () => {
     <div className="App">
       <Row title="Netfilx Originals" fetchUrl={requests.fetchNetfilxOriginal} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
