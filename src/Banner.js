d8773bbc8cd1466e6793807ba21396c8ce469975
ggsuccess <ggsuccess@naver.com>
2020-07-21 18:46:16 +0900

    banner

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import './Banner.css';
 
 const Banner = () => {
   const [movie, setMovie] = useState([]);
@@ -16,21 +17,29 @@ const Banner = () => {
     };
     fetchData();
   }, []);
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + '...' : str;
+  };
+
   return (
     <header
       className="banner"
       style={{
         backgroundSize: 'cover',
-        backgroundImage: `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`,
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
         backgroundPosition: 'center center',
       }}
     >
       <div className="banner-contents">
-        <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className="banner-buttons">
-          <button className="banner-button"></button>
-          <button className="banner-button"></button>
+          <button className="banner-button">Play</button>
+          <button className="banner-button">My List</button>
         </div>
+        <h1 className="banner-description">{truncate(movie?.overview, 150)}</h1>
       </div>
     </header>
   );
