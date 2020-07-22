1bceb402fe2d62f093649793e8b6e3147e630d1b
ggsuccess <ggsuccess@naver.com>
2020-07-22 21:02:44 +0900

    navbarcssSet

--- src/Nav.js
+++ src/Nav.js
@@ -2,14 +2,31 @@ import React from 'react';
 import './Nav.css';
 
 const Nav = () => {
+  const [show, handleshow] = React.useState(false);
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleshow(true);
+      } else {
+        handleshow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && 'nav-black'}`}>
       <img
         className="nav-logo"
-        src="https://upload.wikimedia.org/wikipedia/0/0f"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo Netflix.png"
         alt="netflix logo"
       />
-      <img className="nav-avatar" src="" alt="netflix logo" />
+      <img
+        className="nav-avatar"
+        src="https://pbs.twimg.com/profile_images/12401199990411550720/hBEe3tdn 400x400.png"
+        alt="netflix logo"
+      />
     </div>
   );
 };
